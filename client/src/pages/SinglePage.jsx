a6b31c491b750d3a02ba8e5cb1bbf156ab01b72d
drmaksymov <85283121+drmaksymov@users.noreply.github.com>
2022-11-09 16:52:28 +0200

    SinglePage String to HTML
    SinglePage String to HTML
    розробив можливість конвериуваати string в РЕЬД

--- client/src/pages/SinglePage.jsx
+++ client/src/pages/SinglePage.jsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from "react"
 import { useParams, useNavigate } from "react-router-dom"
 /* import { Link } from "react-router-dom" */
 
-//import './App.css'
 
 
 
@@ -11,7 +10,6 @@ export const SinglePage = () => {
 
 const {id} = useParams();
 const [post, setPost] = useState(null);
-const [html, setHtml] = useState(null);
 
 const navigate = useNavigate();
 const goBack = () => navigate(-1);
@@ -26,38 +24,25 @@ useEffect(()=> {
 },[id]);
   
 
-function ConvertStringToHTML (str) {
-  let parser = new DOMParser();
-  let doc = parser.parseFromString(str, 'text/html');
-  return doc.body;
-};
-const text = ConvertStringToHTML(post.content.rendered);
-
-useEffect (
-  (str) => {
-    let parser = new DOMParser();
-    let doc = parser.parseFromString(str, 'text/html');
-    return doc.body;
-  },
-    
-  
-   [id]
-)
-//
-
+console.log();
 
   return (
     <div>
-      <button onClick={goBack}>D</button>
+      <button  onClick={goBack}
+  class="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+  href="/download"
+>
+  Назад
+</button>
+      
 
       {post && (
       <>
       <h1>{post.title.rendered}</h1>
-       <div>{html}dsdf</div>
+      <p dangerouslySetInnerHTML= {{__html: ` ${post.content.rendered}`}}></p>
        </>
        )
        }
-      
              
     </div>
   )
