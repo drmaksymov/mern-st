df5a84bb442247fced5b724d3c050316e593e48b
drmaksymov <85283121+drmaksymov@users.noreply.github.com>
2022-11-16 13:01:08 +0200

    вав

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -9,7 +9,25 @@ export const AuthPage = () => {
     
   return (
     <div>
-       <h1>Auth Page</h1>
+       <h1 className="font">Auth Paged</h1>
+       
+      {/*  <div className="w-[500px] min-h-[500px] bg-slate-400 p-4 flex  flex-row-reverse items-center space-x-4">
+        <div className=" bg-yellow-500 flex h-[80px] w-[80px]  text-white items-center justify-center">1</div>
+        <div className="bg-black flex h-[80px] w-[80px]  text-white items-center justify-center">2</div>
+        <div className="bg-red-500 flex h-[80px] w-[80px]  text-white items-center justify-center">3</div>
+       </div> */}
+
+
+       <div className="min-h-[600px] bg-slate-300">
+         <div className="container mx-auto flex flex-col sm:flex-row">
+        <div className="w-full sm:w-1/2 min-h-[300px] sm:min-h-[600px] bg-slate-400 justify-center flex items-center flex-col">
+          <h1 className="font-Russo">Сучасна стоматологія в Полтаві</h1>
+          <h2>Сучасна стоматологія в Полтаві</h2>
+        </div>
+        <div className="w-full sm:w-1/2 min-h-[600px] bg-slate-500 ">
+          <img className="" src='https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg' alt="" /></div>
+        </div>
+       </div>
     </div>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+   
+    },
+    extend: { 
+       fontFamily: {
+        Russo: "'Russo One', sans-serif;",
+      },
   },
   plugins: ['@tailwindcss/forms',],
 }
