bfc9bcd1e4d8465847327b049c310132dba34b2f
drmaksymov <85283121+drmaksymov@users.noreply.github.com>
2022-11-19 13:05:20 +0200

    classes

--- client/src/compMy/blockComponents/ModalPopUp.jsx
+++ client/src/compMy/blockComponents/ModalPopUp.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 
+
 //import './App.css'
+import classes from './../styleModule/ModalPopUp.module.css'
+import { Link } from 'react-router-dom';
 
 
 
@@ -10,46 +13,45 @@ export const ModalPopUp = ({CloseModal}) => {
   return (
     <div className="transition duration-150 ease-in-out w-full h-full bg-sky-900 fixed top-0  left-0 flex justify-center items-center bg-opacity-40 filter myblur" onClick={CloseModal} >
        
-       <div onClick={e => e.stopPropagation()} className="gap-4 p-4 w-[400px] h-[350px] bg-white rounded-md flex justify-start flex-col items-center drop-shadow-lg">
+       <div onClick={e => e.stopPropagation()} className="gap-4 p-4 w-[400px] min-h-[350px] bg-white rounded-md flex justify-start flex-col items-center drop-shadow-lg">
        <span className="font-Russo">Записатися на консультацію</span>
        
-       <label
-  for="UserEmail"
-  class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
->
-  <input
-    type="email"
-    id="UserEmail"
-    placeholder="Email"
-    class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-  />
-
-  <span
-    class="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs"
-  >
-    Email
-  </span>
-</label>
 
 
-<label
-  for="UserEmail"
-  class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
->
-  <input
-    type="email"
-    id="UserEmail"
-    placeholder="Email"
-    class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-  />
 
-  <span
-    class="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs"
-  >
-    Email
-  </span>
-</label>
+
+
+      
+
+<input  type="email"   id="UserEmail"   placeholder="Email"   className="input"  />
+<input  type="text"   id="UserEmail"   placeholder="Name"   className="input"  />
+
+<div className="w-full ">
+<label className=""> Дата консультації</label>
+<input  type="date"   id="UserEmail"   placeholder="Name"   className="input"  />
+</div>
+
+
+<div className="w-full ">
+<label className=""> Дата консультації</label>
+<input  type="email"   id="UserEmail"   placeholder="Name"   className="input"  />
+</div>
        
+
+<Link
+  class="w-full flex justify-center group rounded relative inline-block overflow-hidden border border-indigo-600 px-8 py-3 focus:outline-none focus:ring"
+  href="/download"
+>
+  <span
+    class=" absolute inset-y-0 left-0 w-[2px] bg-indigo-600 transition-all group-hover:w-full group-active:bg-indigo-500"
+  ></span>
+
+  <span
+    class="relative text-sm font-medium text-indigo-600 transition-colors group-hover:text-white"
+  >
+    Відправити
+  </span>
+</Link>
        </div>
        
       
